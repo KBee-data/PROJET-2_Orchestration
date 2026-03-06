4dbc9d5a978f657e63a9e631d4b8e2f43a53f61b
KBee-Data <kimberly.n.black@gmail.com>
2026-03-06 08:31:14 +0100

    update python version to 3.11 changed front landing page

--- app_front/main.py
+++ app_front/main.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import requests
-
-st.title('MLOps Project 2 - API Ping')
+#from frontend_logger import get_logger
 
 APP_API_URL = "http://app_api:8000/health"
 
-try:
-    response = requests.get(APP_API_URL)
-    if response.status_code == 200:
-        st.success(f"API is up! Status code {response.status_code}")
-    else:
-        st.error(f"API responded with status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    st.error(f"Failed to reach API: {e}")
+st.title('MLOps Project 2 - API Ping')
+
+st.subheader("Checking the API")
+
+if st.button("Ping the API route"):
+    #logger.info("User clicked the Ping API button")
+    try:
+        response = requests.get(APP_API_URL)
+        if response.status_code == 200:
+            st.success(f"API is up! Status code {response.status_code}")
+            st.code(f"HTTP status: {response.status_code}")
+        else:
+            st.error(f"API responded with status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        st.error(f"Failed to reach API: {e}")
